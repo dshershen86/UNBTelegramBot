639259ca4ee24861a2b3f9a71409599040fa543a
Shershen Dmytro <d.shershen@unb.ua>
2021-07-07 10:09:21 +0300

    New commit

---
+++ src/main/java/com/dshershen/telegram/app/App.java
@@ -0,0 +1,21 @@
+package com.dshershen.telegram.app;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+
+// Аннотация, которая объединяет в себя @Configuration, @EnableAutoConfiguration, @ComponentScan
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        try {
+            TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/dshershen/telegram/bot/BotUNB.java
@@ -0,0 +1,41 @@
+package com.dshershen.telegram.bot;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.bots.TelegramWebhookBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+public class BotUNB extends TelegramLongPollingBot {
+    @Value("${bot.name}")
+    private String botUsername;
+
+    @Value("${bot.token}")
+    private String botToken;
+
+    /* Перегружаем метод интерфейса LongPollingBot
+    Теперь при получении сообщения наш бот будет отвечать сообщением Hi!
+     */
+    @Override
+    public void onUpdateReceived(Update update) {
+        try {
+            SendMessage message = new SendMessage();
+            message.setChatId(String.valueOf(update.getMessage().getChatId()));
+            message.setText("Hi");
+            execute(message);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Геттеры, которые необходимы для наследования от TelegramLongPollingBot
+    public String getBotUsername() {
+        return botUsername;
+    }
+
+    public String getBotToken() {
+        return botToken;
+    }
+}
