0bc2e1a5f21fc40c7e3bb090a293b93b2d5719bf
Shershen Dmytro <d.shershen@unb.ua>
2021-07-07 11:37:24 +0300

    New commit

--- src/main/java/com/dshershen/telegram/App.java
+++ src/main/java/com/dshershen/telegram/App.java
@@ -1,21 +1,13 @@
 package com.dshershen.telegram;
 
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.telegram.telegrambots.meta.TelegramBotsApi;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
-
 
 // Аннотация, которая объединяет в себя @Configuration, @EnableAutoConfiguration, @ComponentScan
-
+@SpringBootApplication
 public class App {
     public static void main(String[] args) {
-        try {
-            TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
-
-        } catch (TelegramApiException e) {
-            e.printStackTrace();
-        }
+        SpringApplication.run(App.class, args);
     }
 }
